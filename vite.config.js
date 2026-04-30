2f1c68586bb1ad682ebd176f658e4d164ff6bbf2
zhaojingjing593 <sgxfzjj@163.com>
2026-04-30 20:54:17 +0800

    Deploy updated files

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig(({ mode }) => ({
   plugins: [react()],
-  base: mode === 'electron' ? './' : '/',
+  base: mode === 'electron' ? './' : './',
   build: {
     outDir: mode === 'electron' ? 'dist/renderer' : 'dist/web',
     emptyOutDir: true,
